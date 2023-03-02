7c64927be23bdaaec7906d9d35598171d0e3b472
zahara-Rahmanchy <113811254+zahara-Rahmanchy@users.noreply.github.com>
2023-03-02 11:24:37 +0600

    Update Navigation.js

--- diabetesprediction/src/components/Navigation.js
+++ diabetesprediction/src/components/Navigation.js
@@ -1,33 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 // flex justify-between items-center
 export const Navigation = () => {
   return (
     <>
-    <nav className='grid grid-cols-4 items-center  bg-red-200 space-x-64'>
-
+      <nav className="grid grid-cols-4 items-center  bg-red-200 space-x-64">
         <div className="py-3 w-11 bg-slate-300">
-            <h1 className="text-5xl text-rose-500 font-bold px-4 py-2">LOGO</h1>
-        </div>
-     
-      <ul className="flex justify-between space-x-40 p-2 col-span-3">
-        <div className=' flex justify-evenly '>
-        <li className="text-lg font-medium p-2 my-5 mx-7 " > <Link to="/">Home </Link></li>
-         <li className="text-lg font-medium p-2 my-5 mx-7 "> <Link to="/Prediction">Prediction </Link></li>
-        <li className="text-lg font-medium p-2 my-5 mx-7"> <Link to="/Maintanence"> Maintenance </Link></li>
+          <h1 className="text-5xl text-rose-500 font-bold px-4 py-2">LOGO</h1>
         </div>
 
-        {/* login/sign up */}
-        <div className='col-span-1  flex justify-self-end'>
-        <li className='text-lg font-medium p-2 my-5 mx-7 '> <Link to="/Login">Login </Link></li>
+        <ul className="flex justify-between space-x-40 p-2 col-span-3">
+          <div className=" flex justify-evenly ">
+            <li className="text-lg font-medium p-2 my-5 mx-7 ">
+              {" "}
+              <Link to="/">Home </Link>
+            </li>
+            <li className="text-lg font-medium p-2 my-5 mx-7 ">
+              {" "}
+              <Link to="/Prediction">Prediction </Link>
+            </li>
+            <li className="text-lg font-medium p-2 my-5 mx-7">
+              {" "}
+              <Link to="/Maintanence"> Maintenance </Link>
+            </li>
+          </div>
 
-        <li className='text-lg font-medium p-2 my-5 mx-7 '> <Link>SignUp </Link></li>
-        </div>
-       </ul> 
+          {/* login/sign up */}
+          <div className="col-span-1  flex justify-self-end">
+            <li className="text-lg font-medium p-2 my-5 mx-7 ">
+              {" "}
+              <Link to="/Login">Login </Link>
+            </li>
 
-        
-       </nav>
-    
+            <li className="text-lg font-medium p-2 my-5 mx-7 ">
+              {" "}
+              <Link>SignUp </Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
     </>
-  )
-}
+  );
+};
+
